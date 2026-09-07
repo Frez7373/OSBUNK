a648168a5e70ee1662f922a52b3c76b3d125bfe2
Frez7373 <danilla.mikhailov@gmail.com>
2026-09-07 22:40:36 +0300

    Build OSBUNK startup loader

--- startup.lua
+++ startup.lua
@@ -1,1 +1,18 @@
-print("OSBUNK")
+-- OSBUNK startup loader
+local ok, err = pcall(function()
+    if not fs.exists("/OSBUNK/boot.lua") then
+        error("OSBUNK is not installed. Run: wget run https://raw.githubusercontent.com/Frez7373/OSBUNK/main/install.lua")
+    end
+    shell.run("/OSBUNK/boot.lua")
+end)
+if not ok then
+    term.setBackgroundColor(colors.black)
+    term.setTextColor(colors.red)
+    term.clear()
+    term.setCursorPos(1, 1)
+    print("OSBUNK BOOT ERROR")
+    print("")
+    print(err)
+    print("")
+    print("Run the installer again to repair the system.")
+end
